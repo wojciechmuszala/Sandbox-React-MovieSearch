36871135932c0cbe074ac65759d56ef0f487ccb2
Wojciech Muszala <wojciech.muszala@gmail.com>
2025-05-28 19:38:52 +0200

    feat(header): add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 const App = () => {
-  return <main></main>;
+  return (
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' alt='Hero Banner' />
+          <h1>
+            Find <span className='text-gradient'>Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
 };
 
 export default App;
